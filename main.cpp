7bb2cf100d1e3a686b07324d0333780cc4d69df3
moculll <823309460@qq.com>
2025-05-28 01:57:59 +0800

    deleted useless line, add release build

--- main.cpp
+++ main.cpp
@@ -1,14 +1,4 @@
 ﻿#include "ScrewVivoTWS.h"
-#include "MTrayMenu/MTrayMenu.h"
-#include <cstdint>
-#include <future>
-#include <QtConcurrent>
-#include <QSystemTrayIcon>
-#include <QMenu>
-#include <QAction>
-#include <QIcon>
-#include <QObject>
-#include <QCoreApplication>
 #include <QApplication>
 int main(int argc, char *argv[])
 {
